a8626a5e844d4387db622289ecea128b9836cb6f
Faik Turan <turanfaik@hotmail.com>
2021-12-09 16:31:20 +0300

    Add project files.

---
+++ MCD_DinamikFormElemanlari/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace MCD_DinamikFormElemanlari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMayin = new System.Windows.Forms.Label();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(62, 46);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(660, 290);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(194, 9);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(13, 13);
+            this.lblSkor.TabIndex = 0;
+            this.lblSkor.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SKOR:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MAYIN:";
+            // 
+            // lblMayin
+            // 
+            this.lblMayin.AutoSize = true;
+            this.lblMayin.Location = new System.Drawing.Point(263, 9);
+            this.lblMayin.Name = "lblMayin";
+            this.lblMayin.Size = new System.Drawing.Size(13, 13);
+            this.lblMayin.TabIndex = 3;
+            this.lblMayin.Text = "0";
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(282, 4);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(75, 23);
+            this.btnUret.TabIndex = 4;
+            this.btnUret.Text = "Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUret);
+            this.Controls.Add(this.lblMayin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Dinamik Form Elemanları";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMayin;
+        private System.Windows.Forms.Button btnUret;
+    }
+}
+
